3edf50891400ee2d2be1445b577976fa0bb82dd0
Ura Pasternak <yra.pasternak@gmail.com>
2020-05-22 18:35:40 +0300

    add DAO

---
+++ src/main/java/ua/lviv/ura/univer/dao/FacultetRepository.java
@@ -0,0 +1,9 @@
+package ua.lviv.ura.univer.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ua.lviv.ura.univer.domain.Facultet;
+
+public interface FacultetRepository extends JpaRepository<Facultet, Integer> {
+
+}
---
+++ src/main/java/ua/lviv/ura/univer/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package ua.lviv.ura.univer.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ua.lviv.ura.univer.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
